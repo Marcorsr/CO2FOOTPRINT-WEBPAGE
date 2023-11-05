d3915efe17a19e9d4d6e518e15e288657c433c83
paletta93 <paletta93@web.de>
2023-11-05 15:06:10 +0100

    Tabellenfilter hinzugefügt, Radiobuttons für Schriftkulturauswahl hinzugefügt.

--- myScript.js
+++ myScript.js
@@ -52,3 +52,41 @@ function sortTable(n) {
     }
   }
 }
+
+
+function myFunction() {
+  var input, filter, table, tr, td, i, txtValue;
+  var ersterFilter
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("sortable");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+       ersterFilter = "true";
+      }else {
+               tr[i].style.display = "none";
+    }
+  }
+
+
+
+} if(!ersterFilter){
+
+ for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[1];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      }else {
+               tr[i].style.display = "none";
+    }
+  }
+}
+}
+}
